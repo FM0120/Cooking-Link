0651e56bfb5df6287bd3376c1f46bdee8b223f9a
FM0120 <kaz.fumiaki@gmail.com>
2021-11-25 01:31:50 +0000

    レイアウトの修正

--- app/controllers/makings_controller.rb
+++ app/controllers/makings_controller.rb
@@ -1,22 +1,3 @@
 class MakingsController < ApplicationController
-  # def new
-  #   @making = Makeing.new
-  #   @making = @recipe.making.build ##親モデル.子モデル.buildで子モデルのインスタンス作成
-  # end
-
-  # def create
-  #   @making = Makeing.new(making_params)
-  #   if @making.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # private
-
-  # def recipe_params
-  #   params.require(:recipe).permit(:recipe_title, :recipe_detail, :trick, :food, :image,
-  #   making:[:makeing,:makeing_number,:image,:recipe_id])
-  # end
+ 
 end
